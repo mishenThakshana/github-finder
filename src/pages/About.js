d932b72cb1d5fdf1ef9e0de3e5bb6b85f88aa71d
Mishen Thakshana <102714819+mishenThakshana@users.noreply.github.com>
2022-08-31 22:28:30 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+
+      <p className="mb-4 text-2xl font-light">
+        Made in ❤️ by
+        <strong>
+          <a href="#"> Mishen Thakshana</a>
+        </strong>
+        .
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+    </>
+  );
+};
+
+export default About;
